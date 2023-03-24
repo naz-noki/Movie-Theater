ad0dbbe20fc04c2d635fb7786bf1add4a9028dbf
NoK1 <nazargolcheko@gmail.com>
2023-03-25 02:15:08 +0300

    Create project

---
+++ src/components/homePage.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="home__wrapper">
+        <div class="home__block">
+            <div class="home__text">Вы хотите заплатить за коммунальные услуги?</div>
+        </div>
+    </div>
+</template>
